51101e4fa44912a2c33e39a9a1a32dae7ca07007
ScriptersOfTheDuckSanctuary <theducksanctuary11@gmail.com>
2026-01-08 16:29:58 -0600

    autonomous update (more efficient)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoLeft.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoLeft.java
@@ -8,39 +8,52 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 @Autonomous(name="DECODE Auto Up", group="DECODE")
-public class AutoLeft extends LinearOpMode{
+public class AutoLeft extends LinearOpMode {
+
+    DcMotor motorFL, motorFR, motorBL, motorBR;
+
     @Override
     public void runOpMode() {
+
+        // ---------------------------
+        // Hardware Mapping
+        // ---------------------------
+        motorFL = hardwareMap.get(DcMotor.class, "motorFL");
+        motorFR = hardwareMap.get(DcMotor.class, "motorFR");
+        motorBL = hardwareMap.get(DcMotor.class, "motorBL");
+        motorBR = hardwareMap.get(DcMotor.class, "motorBR");
+
+        motorFL.setDirection(DcMotor.Direction.REVERSE);
+        motorFR.setDirection(DcMotor.Direction.FORWARD);
+        motorBL.setDirection(DcMotor.Direction.REVERSE);
+        motorBR.setDirection(DcMotor.Direction.FORWARD);
+
         telemetry.addData("Status", "Initialized");
         telemetry.update();
+
         waitForStart();
-        while (opModeIsActive()) {
-            // ---------------------------
-            // Hardware Mapping
-            // ---------------------------
-            DcMotor motorFL = hardwareMap.get(DcMotor.class, "motorFL");
-            DcMotor motorFR = hardwareMap.get(DcMotor.class, "motorFR");
-            DcMotor motorBL = hardwareMap.get(DcMotor.class, "motorBL");
-            DcMotor motorBR = hardwareMap.get(DcMotor.class, "motorBR");
-            motorFL.setDirection(DcMotor.Direction.REVERSE);
-            motorFR.setDirection(DcMotor.Direction.FORWARD);
-            motorBL.setDirection(DcMotor.Direction.REVERSE);
-            motorBR.setDirection(DcMotor.Direction.FORWARD);
-            while (getRuntime() < 6) {
-                if (getRuntime() > 5) {
-                    motorBR.setPower(1);
-                    motorBL.setPower(1);
-                    motorFR.setPower(1);
-                    motorFL.setPower(1);
-                }
-            }
+        resetRuntime();
+
+        // ---------------------------
+        // Drive forward for 1 second
+        // ---------------------------
+        while (opModeIsActive() && getRuntime() < 1.0) {
+            motorFL.setPower(1);
+            motorFR.setPower(1);
+            motorBL.setPower(1);
+            motorBR.setPower(1);
 
             telemetry.addData("Runtime", getRuntime());
-            telemetry.addData("FL", motorFL.getPower());
-            telemetry.addData("FR", motorFR.getPower());
-            telemetry.addData("BL", motorBL.getPower());
-            telemetry.addData("BR", motorBR.getPower());
             telemetry.update();
         }
+
+        // ---------------------------
+        // Stop motors
+        // ---------------------------
+        motorFL.setPower(0);
+        motorFR.setPower(0);
+        motorBL.setPower(0);
+        motorBR.setPower(0);
     }
 }
+
